e09ba5e2e2464d03f7b701797707948c4effb410
ryan-parag <parag.ryan@gmail.com>
2018-06-26 12:48:48 -0400

    added imagemin, decreased table section padding

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,25 @@
-var gulp 				= require('gulp'),
-		mjml				=	require('gulp-mjml'),
-		browserSync = require('browser-sync');
+var gulp = require('gulp'),
+  mjml = require('gulp-mjml'),
+  imagemin = require('gulp-imagemin')
+browserSync = require('browser-sync');
 
-gulp.task('mjml', function () {
-  return gulp.src('templates/**/*.mjml')
+gulp.task('mjml', function() {
+  return gulp.src('templates/*.mjml')
     .pipe(mjml())
     .pipe(gulp.dest('output/'))
 })
 
-gulp.task('watch', function (){
-	gulp.watch('templates/**/*.mjml', ['mjml']);
+gulp.task('images', () =>
+  gulp.src('templates/img/*')
+  .pipe(imagemin())
+  .pipe(gulp.dest('output/img'))
+);
+
+gulp.task('watch', function() {
+  gulp.watch('templates/**/*.mjml', ['mjml']);
 });
 
-gulp.task('browser-sync', ['mjml'], function () {
+gulp.task('browser-sync', ['mjml'], function() {
   browserSync({
     server: {
       baseDir: 'output/'
@@ -21,4 +28,4 @@ gulp.task('browser-sync', ['mjml'], function () {
   });
 });
 
-gulp.task('default', ['mjml','browser-sync', 'watch']);
+gulp.task('default', ['mjml', 'browser-sync', 'watch', 'images']);
