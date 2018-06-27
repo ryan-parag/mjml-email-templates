e3ed87950a53ca45584b1301b40a4cf40917c616
ryan-parag <parag.ryan@gmail.com>
2018-06-27 16:26:59 -0400

    fixed gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp'),
-  mjml = require('gulp-mjml'),
-  imagemin = require('gulp-imagemin')
-browserSync = require('browser-sync');
+	  mjml = require('gulp-mjml'),
+	  imagemin = require('gulp-imagemin')
+		browserSync = require('browser-sync');
 
 gulp.task('mjml', function() {
   return gulp.src('templates/*.mjml')
@@ -9,23 +9,24 @@ gulp.task('mjml', function() {
     .pipe(gulp.dest('output/'))
 })
 
-gulp.task('images', () =>
-  gulp.src('templates/img/*')
-  .pipe(imagemin())
-  .pipe(gulp.dest('output/img'))
-);
+gulp.task('imageOpt', function () {
+	gulp.src('templates/img/*')
+		.pipe(imagemin())
+		.pipe(gulp.dest('output/img'))
+})
 
-gulp.task('watch', function() {
-  gulp.watch('templates/**/*.mjml', ['mjml']);
-});
+gulp.task('watch',['browserSync'], function(){
+  gulp.watch('templates/**/*.mjml', ['mjml'], browserSync.reload);
+})
 
-gulp.task('browser-sync', ['mjml'], function() {
+gulp.task('browserSync', function() {
   browserSync({
     server: {
       baseDir: 'output/'
     },
-    notify: false
-  });
-});
+  })
+})
 
-gulp.task('default', ['mjml', 'browser-sync', 'watch', 'images']);
+gulp.task('build', ['imageOpt', 'mjml'])
+
+gulp.task('default', ['build', 'watch']);
